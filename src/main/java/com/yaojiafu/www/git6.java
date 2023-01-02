f3175d3728a53e4c3635f8105ce0495febeda817
yao <939861608@qq.com>
2023-01-02 12:59:39 +0800

    master分支git6第二次提交

--- src/main/java/com/yaojiafu/www/git6.java
+++ src/main/java/com/yaojiafu/www/git6.java
@@ -3,5 +3,6 @@ package com.yaojiafu.www;
 public class git6 {
     public static void main(String[] args) {
         System.out.println("sadfasd01");
+        System.out.println("sadfasd02");
     }
 }

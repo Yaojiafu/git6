3e07c26aa72eddc551299741f730cda17f5f4f67
yao <939861608@qq.com>
2023-01-02 13:00:36 +0800

    hot-fix分支git6第一次提交

--- src/main/java/com/yaojiafu/www/git6.java
+++ src/main/java/com/yaojiafu/www/git6.java
@@ -4,5 +4,6 @@ public class git6 {
     public static void main(String[] args) {
         System.out.println("sadfasd01");
         System.out.println("sadfasd02");
+        System.out.println("sadfasd03");
     }
 }
